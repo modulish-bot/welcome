d4f734d7148dd35b66d2354c376e12e529f02ac7
Daniel <danilacasito8@gmail.com>
2025-09-07 17:19:52 +0000

    Add welcome plugin with configuration and initialization logic

---
+++ welcome.py
@@ -0,0 +1,24 @@
+import discord, os, shutil, json
+from discord.ext import commands
+
+class Welcome(commands.Cog):
+    
+    def __init__(self, bot: discord.Bot, config_dir: str):
+        self.bot = bot
+        self.config_dir = config_dir
+        self.config = json.loads(open(config_dir + "/config.json", "r"))
+        if self.config["destination_channel_id"] == 0:
+            print("[ERROR] Channel ID not set, disabling...")
+            self.bot.remove_cog("Welcome")
+
+
+
+def setup(bot: discord.Bot, config_dir: str = None):
+    if config_dir is None:
+        config_dir = globals().get('PLUGIN_CONFIG_DIR', '<unknown>')
+
+    if not os.path.exists(config_dir + "/config.json"):
+        shutil.copy2("config.json", config_dir + "/config.json")
+
+    
+    bot.add_cog(Welcome(bot,config_dir))
